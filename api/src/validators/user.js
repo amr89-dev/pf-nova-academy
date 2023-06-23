fe8ac17c767d64aa3172a2d7ab42d937dff3ad9f
santiagoweb212 <juanusca45@gmail.com>
2023-06-22 19:24:54 -0500

    validaciones -user -expressvalidator

---
+++ api/src/validators/user.js
@@ -0,0 +1,25 @@
+const { check } = require("express-validator");
+const validateResult = require("../helpers/validateResult");
+const validateUser = [
+  check("name").exists().not().isEmpty().withMessage("the name is required"),
+  check("email")
+    .exists()
+    .not()
+    .isEmpty()
+    .withMessage("the email is required")
+    .isEmail(),
+  check("password")
+    .exists()
+    .not()
+    .isEmpty()
+    .withMessage("the password is required")
+    .isLength({ min: 8 })
+    .withMessage("the password must have at least 8 characters")
+    .matches(/\d/)
+    .withMessage("the password must contain at least one number")
+    .matches(/[A-Z]/)
+    .withMessage("the password must contain at least one uppercase letter"),
+  (req, res, next) => {
+    validateResult(req, res, next);
+  },
+];
