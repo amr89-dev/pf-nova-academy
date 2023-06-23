3872ef8f01e110a9127c5a752d3434bc0a032232
NeiDer1123 <neybm1123@gmail.com>
2023-06-22 19:44:38 -0500

    Controllers

---
+++ api/src/controllers/lesson.controller.js
@@ -0,0 +1,75 @@
+const { Lesson } = require("../models");
+
+const createLesson = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const lesson = await Lesson.create({ title, content });
+    res.json(lesson);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error creating lesson" });
+  }
+};
+
+const getLessons = async (req, res) => {
+  try {
+    const lessons = await Lesson.findAll();
+    res.json(lessons);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error retrieving lessons" });
+  }
+};
+
+const getLessonById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const lesson = await Lesson.findByPk(id);
+    if (!lesson) {
+      return res.status(404).json({ error: "Lesson not found" });
+    }
+    res.json(lesson);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error retrieving lesson" });
+  }
+};
+
+const updateLessonById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    const lesson = await Lesson.findByPk(id);
+    if (!lesson) {
+      return res.status(404).json({ error: "Lesson not found" });
+    }
+    await lesson.update({ title, content });
+    res.json(lesson);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error updating lesson" });
+  }
+};
+
+const deleteLessonById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const lesson = await Lesson.findByPk(id);
+    if (!lesson) {
+      return res.status(404).json({ error: "Lesson not found" });
+    }
+    await lesson.destroy();
+    res.json({ message: "Lesson deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error deleting lesson" });
+  }
+};
+
+module.exports = {
+  createLesson,
+  getLessons,
+  getLessonById,
+  updateLessonById,
+  deleteLessonById,
+};
