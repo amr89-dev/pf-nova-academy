45a44f3a073fb085da5a3d9ba0d7937532ed8087
santiagoweb212 <juanusca45@gmail.com>
2023-06-22 21:36:03 -0500

    update atributos user

--- api/src/models/user.js
+++ api/src/models/user.js
@@ -7,31 +7,29 @@ module.exports = (sequelize) => {
       id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true
+        autoIncrement: true,
       },
-      firstName: {
+      name: {
         type: DataTypes.STRING(50),
-        allowNull: false
+        allowNull: false,
       },
-      lastName: {
-        type: DataTypes.STRING(50),
-        allowNull: false
-      },
+
       email: {
         type: DataTypes.STRING(100),
-        allowNull: false
+        allowNull: false,
       },
       password: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
       },
       role: {
         type: DataTypes.INTEGER,
-        allowNull: false
-      }
-    }, {
-      tableName: 'User',
-      timestamps: false
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "User",
+      timestamps: false,
     }
   );
 };
