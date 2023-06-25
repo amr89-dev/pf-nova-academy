0fcaf222f63f69b5dd463f5feabd4e9d2642373f
sorrus-dev <amr89.dev@gmail.com>
2023-06-25 16:32:19 -0500

    cambiando el nombre

--- client/src/components/CourseCards/CourseCard.jsx
+++
@@ -1,5 +0,0 @@
-const CourseCard = () => {
-  return <div>CourseCard</div>;
-};
-
-export default CourseCard;
---
+++ client/src/components/CourseCards/CourseCards.jsx
@@ -0,0 +1,5 @@
+const CourseCards = () => {
+  return <div>CourseCards</div>;
+};
+
+export default CourseCards;
