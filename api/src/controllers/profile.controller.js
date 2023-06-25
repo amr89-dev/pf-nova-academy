8e3995bcae5767c55dd7d4ceda499b536a53a19b
santiagoweb212 <juanusca45@gmail.com>
2023-06-25 10:59:46 -0500

    get perfil user

---
+++ api/src/controllers/profile.controller.js
@@ -0,0 +1,19 @@
+const { Profile, User } = require("../db");
+const getUserToken = require("../helpers/getUsertoken");
+const getProfile = async (req, res) => {
+  try {
+    const user = await getUserToken(req);
+    const userProfile = await Profile.findOne({
+      where: { userId: user.idUser },
+      include: {
+        model: User,
+        attributes: { exclude: ["password", "userId", "role"] },
+      },
+    });
+    if (!userProfile) throw Error("User profile not found");
+    res.send(userProfile);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+module.exports = { getProfile };
