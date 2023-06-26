0a7ad1cee914e47a31cf0a7882c0a2eab410a0aa
sorrus-dev <amr89.dev@gmail.com>
2023-06-25 19:45:43 -0500

    añadiendo redux

--- client/src/components/CourseCard/CourseCard.jsx
+++ client/src/components/CourseCard/CourseCard.jsx
@@ -2,7 +2,7 @@
 const CourseCard = ({ dataCard }) => {
   const { title, price, description, images, category } = dataCard;
   return (
-    <div className="w-64 h-auto bg-second-purple rounded-md p-3 cursor-pointer">
+    <div className="w-64 h-auto bg-light-gray rounded-md p-3 cursor-pointer">
       <figure className=" relative  h-auto w-full">
         <span className="absolute bottom-0 left-0 bg-[#FFFFFF60] rounded-lg text-black text-xs m-2 px-3 py-0.5 ">
           {category.name}
@@ -13,7 +13,7 @@ const CourseCard = ({ dataCard }) => {
           className="w-full rounded-lg object-cover "
         />
         <button className="absolute top-0 right-0 flex justify-center items-center bg-[#FFFFFF] w-6 h-6 rounded-full m-2 p-2">
-          +
+          ♥️
         </button>
       </figure>
       <p className="flex flex-col justify-between">
---
+++ client/src/redux/reducer/reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+//import dogReducer from "./dogReducer.js";
+
+const reducer = combineReducers({
+  // dogReducer: dogReducer,
+});
+
+export default reducer;
