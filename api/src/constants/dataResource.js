e5ab23fb30ce10decde0403349b8a39ee32100c3
NeiDer1123 <neybm1123@gmail.com>
2023-06-25 19:52:21 -0500

    Datos ficticios para probar la DB

---
+++ api/src/constants/dataResource.js
@@ -0,0 +1,63 @@
+const resource = [
+    {
+      name: "Recurso 1",
+      type: "Tipo 1",
+      link: "https://recurso1.com",
+      lessonId: "3",
+    },
+    {
+      name: "Recurso 2",
+      type: "Tipo 2",
+      link: "https://recurso2.com",
+      lessonId: "2",
+    },
+    {
+      name: "Recurso 3",
+      type: "Tipo 3",
+      link: "https://recurso3.com",
+      lessonId: "4",
+    },
+    {
+      name: "Recurso 4",
+      type: "Tipo 4",
+      link: "https://recurso4.com",
+      lessonId: "2",
+    },
+    {
+      name: "Recurso 5",
+      type: "Tipo 5",
+      link: "https://recurso5.com",
+      lessonId: "7",
+    },
+    {
+      name: "Recurso 6",
+      type: "Tipo 6",
+      link: "https://recurso6.com",
+      lessonId: "6",
+    },
+    {
+      name: "Recurso 7",
+      type: "Tipo 7",
+      link: "https://recurso7.com",
+      lessonId: "1",
+    },
+    {
+      name: "Recurso 8",
+      type: "Tipo 8",
+      link: "https://recurso8.com",
+      lessonId: "8",
+    },
+    {
+      name: "Recurso 9",
+      type: "Tipo 9",
+      link: "https://recurso9.com",
+      lessonId: "4",
+    },
+    {
+      name: "Recurso 10",
+      type: "Tipo 10",
+      link: "https://recurso10.com",
+      lessonId: "1",
+    },
+  ];
+  
