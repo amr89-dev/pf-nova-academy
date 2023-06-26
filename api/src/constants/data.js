e8dede8566f702f583607200ffb536aedd14cdd8
santiagoweb212 <juanusca45@gmail.com>
2023-06-26 15:42:46 -0500

    new cambios relacion de modelos - add cargadores de Db-crud courseforsale...

--- api/src/constants/data.js
+++ api/src/constants/data.js
@@ -1,21 +1,65 @@
+
 const cursos = [
   {
-    name: "Curso de programación en Python",
-    category: "Programación",
-    duration: "40 horas",
-    price: 200,
-  },
-  {
-    name: "Curso de marketing digital",
-    category: "Marketing",
-    duration: "20 horas",
+    name: "Curso de Matemáticas Avanzadas",
+    category: ["Matemáticas"],
+    description: "Aprende conceptos avanzados de matemáticas con este curso.",
     price: 150,
+    duration: "8 semanas",
+    images: ["https://example.com/math-course.jpg"],
   },
   {
-    name: "Curso de diseño gráfico",
-    category: "Diseño",
-    duration: "30 horas",
-    price: 250,
+    name: "Curso de Biología Molecular",
+    category: ["Ciencias Naturales"],
+    description: "Descubre los secretos de la biología molecular en este curso especializado.",
+    price: 180,
+    duration: "10 semanas",
+    images: ["https://example.com/biology-course.jpg"],
+  },
+  // Agrega más cursos aquí...
+  {
+    name: "Curso de Programación Python",
+    category: ["Programación"],
+    description: "Aprende a programar en Python desde cero con este curso práctico.",
+    price: 100,
+    duration: "6 semanas",
+    images: ["https://example.com/python-course.jpg"],
   },
 ];
-module.exports = cursos;
+
+
+
+const category = [
+  { name: "Matemáticas" },
+  { name: "Ciencias Naturales" },
+  { name: "Historia" },
+  { name: "Idiomas" },
+  { name: "Programación" },
+  { name: "Arte y Diseño" },
+  { name: "Literatura" },
+  { name: "Economía" },
+  { name: "Psicología" },
+  { name: "Música" },
+];
+
+// El arreglo 'educationCategories' contiene 10 objetos de categoría relacionados a una plataforma educativa
+const users = [
+  {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    password: "Password123",
+  },
+  {
+    name: "Alice Smith",
+    email: "alice.smith@example.com",
+    password: "Password456",
+  },
+  {
+    name: "Bob Johnson",
+    email: "bob.johnson@example.com",
+    password: "Password789",
+  },
+  
+];
+
+module.exports = { cursos, category, users };
--- api/src/middleware/authRole.js
+++ api/src/middleware/authRole.js
@@ -1,12 +1,11 @@
 const { verifyToken } = require("../helpers/generateToken");
 const { User } = require("../db");
-
 const verificarRole = (role) => async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ").pop();
     const tokenData = await verifyToken(token);
     const userData = await User.findByPk(tokenData.idUser);
-    if ([role].includes(userData.role)) next();
+    if (userData && [role].includes(userData.role)) next();
     else throw Error("User not authorized");
   } catch (error) {
     console.error("error", error);
