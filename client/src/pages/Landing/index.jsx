00c311f4bfdac3667ea98f54d4d56231d8c0110e
danielpaz23 <danielpaz2233@gmail.com>
2023-06-27 01:22:14 -0300

    search

--- client/src/pages/Landing/index.jsx
+++ client/src/pages/Landing/index.jsx
@@ -2,11 +2,13 @@ import Footer from "../../components/Footer/Footer";
 import Carrousel from "../../components/Carrousel";
 import Banner from "../../components/Banner/Banner";
 import Layout from "../../Layout/";
+import SearchBar from "../../components/SearchBar/SearchBar";
 
 const Landing = () => {
   return (
     <Layout>
-      <Banner />
+      <Banner />      
+      <SearchBar />
       <Carrousel />
       <Footer />
     </Layout>
