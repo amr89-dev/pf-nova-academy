4b0acd6a767aa610b77ed4cfd67b0b20b773834d
sorrus-dev <amr89.dev@gmail.com>
2023-06-26 21:05:47 -0500

    listo el primer endpoint

--- client/src/components/CourseCard/CourseCard.jsx
+++ client/src/components/CourseCard/CourseCard.jsx
@@ -4,7 +4,7 @@ import { addToCart } from "../../redux/actions/shoppingCartActions";
 /* eslint-disable */
 
 const CourseCard = ({ dataCard }) => {
-  const { title, price, description, images, category } = dataCard;
+  const { name, price, description, images, category } = dataCard;
   const dispatch = useDispatch();
 
   const handleCart = (data) => {
@@ -20,11 +20,11 @@ const CourseCard = ({ dataCard }) => {
     >
       <figure className=" relative  h-auto w-full">
         <span className="absolute bottom-0 left-0 bg-[#FFFFFF60] rounded-lg text-black text-xs m-2 px-3 py-0.5 ">
-          {category.name}
+          {category[0]}
         </span>
         <img
-          src={images[0]}
-          alt={title}
+          src="https://picsum.photos/800/600"
+          alt={name}
           className="w-full rounded-lg object-cover "
         />
         <button className="absolute top-0 right-0 flex justify-center items-center bg-[#FFFFFF] w-6 h-6 rounded-full m-2 p-2">
@@ -32,7 +32,7 @@ const CourseCard = ({ dataCard }) => {
         </button>
       </figure>
       <p className="flex flex-col justify-between">
-        <span className="text-md font-bold">{title}</span>
+        <span className="text-md font-bold">{name}</span>
         <span>⭐⭐⭐⭐⭐</span>
         <span className="text-lg font-medium">${price}</span>
         <span className="text-sm font-light">{description}</span>
