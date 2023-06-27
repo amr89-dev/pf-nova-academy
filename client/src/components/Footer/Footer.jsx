1bd10315f112ed94ba51ce1f1aa7c701946ffd2b
Agustin <atunez30@gmail.com>
2023-06-27 00:51:14 -0300

    searchbar

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 
+
 const Footer = () => {
   return (
-    <footer className="bg-dark-gray text-primary-blue static bottom-0 w-full h-max">
-      <div className="p-2  h-auto text-gray-200">
+    <footer className="bg-dark-gray text-primary-blue fixed bottom-0 w-full">
+      <div className="p-2 h-auto text-gray-200">
         <div className="max-w-7xl mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
-            
             <div className="mb-5">
               <ul>
                 <li className="pb-4 relative">
@@ -28,13 +28,12 @@ const Footer = () => {
               </ul>
             </div>
             <div className="mb-5">
-  <span className="mr-2">Unete a nuestro Boletin Informativo</span>
-  
-  <form className="mt-5">
-    <input type="text" name="" id="" />
-    <button className="ml-2">Suscribete</button>
-  </form>
-</div>
+              <span className="mr-2">Unete a nuestro Boletin Informativo</span>
+              <form className="mt-5">
+                <input type="text" name="" id="" />
+                <button className="ml-2">Suscribete</button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
---
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import { Footer } from "../Footer/Footer";
+
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Bienvenido a Nova ACADEMY</h1>
+      <p>¡Encuentra los mejores cursos para ampliar tus conocimientos!!!</p>
+      <Footer/>
+    </div>
+  );
+};
+
+export default LandingPage;
